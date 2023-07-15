9269f0d5b501dd641a3207d1293fab15036b24c1
Thoroughfare by the Brooks <diptonilr@gmail.com>
2023-07-15 23:21:42 +0530

    Set up dependencies

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<html lang="en">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Chasse: Simplifying HTML Boilerplates</title>
+    </head>
+
+    <body>
+        <p class="text-teal-600">Hey</p>
+    </body>
+</html>
