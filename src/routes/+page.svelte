4e38873a1f0d07d89bf49b8b3eb6f9393b027d0a
Thoroughfare by the Brooks <diptonilr@gmail.com>
2023-07-17 03:34:26 +0530

    Finish Scrollbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,6 @@
 <script>
     import Icon from "$lib/assets/chasse.png";
-    import { BootstrapBrand, SassBrand, WindSolid, GithubBrand } from 'svelte-awesome-icons';
-
-    let clicked = false;
+    import { BootstrapBrand, SassBrand, WindSolid, GithubBrand, BarsSolid } from 'svelte-awesome-icons';
 </script>
 
 
@@ -10,11 +8,9 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Chasse: Simplifying HTML Boilerplates</title>
-        <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-        <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     </head>
 
-    <body class="select-none">
+    <body>
         <header>
             <nav class="flex items-center justify-between w-[92%] mx-auto">
                 <div>
@@ -24,14 +20,14 @@
                     <ul class="flex md:flex-row flex-col md:items-end text-teal-600 gap-16 text-xl">
                         <li class="hover:underline underline-offset-32 decoration-2 hover:text-teal-500"><a href="www">Learn</a></li>
                         <li class="hover:underline underline-offset-32 decoration-2 hover:text-teal-500"><a href="www">Conventions</a></li>
-                        <li class="hover:underline underline-offset-32 decoration-2 hover:text-teal-500"><a href="www">Framework Guides</a></li>
                         <li class="hover:underline underline-offset-32 decoration-2 hover:text-teal-500"><a href="www">Get Involved</a></li>
                     </ul>
                 </div>
                 <div class="flex items-center gap-6">
+                    <a href="https://github.com/Diptonil/chasse"><GithubBrand class="text-gray-500 h-8 w-8" /></a>
                     <button class="bg-slate-950 text-teal-600 px-5 py-2 rounded-full border-2 border-teal-600 hover:text-teal-500 hover:border-teal-500 shadow-md hover:shadow-teal-600">Install</button>
                     <button class="bg-teal-600 text-slate-950 px-5 py-2 rounded-full hover:bg-teal-500 shadow-md hover:shadow-teal-600">Get Started</button>
-                    <ion-icon on:click={ clicked = !clicked } class:top-[9%]={ clicked } class="text-teal-600 text-4xl cursor-pointer md:hidden" name="menu-outline"></ion-icon>
+                    <BarsSolid class="text-teal-600 text-4xl cursor-pointer md:hidden" name="menu-outline" />
                 </div>
             </nav>
         </header>
@@ -88,7 +84,7 @@
                     <p class="w-64 pt-4">Chasse integrates well with most of the CSS frameworks out there that utilize classes within HTML for styling. For custom CSS, it is handled the usual way, or with Sass or Less.</p>
                 </div>
             </div>
-            <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
+            <hr class="h-px my-16 bg-gray-200 border-0 dark:bg-gray-700">
 
         </main>
     </body>
